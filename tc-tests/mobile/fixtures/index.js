c30b87c8afd46c1b573cfec5948f3278a5275d73
nuwansamaware <nuwansam@awarelabs.io>
2026-05-21 09:55:03 +1000

    sync

--- tc-tests/mobile/fixtures/index.js
+++
@@ -1,5 +0,0 @@
-import { test as base } from '@mobilewright/test';
-import { installTestChimp } from '@testchimp/playwright/runtime';
-
-export const test = installTestChimp(base, { uiFixture: 'screen' });
-export { expect } from '@mobilewright/test';
